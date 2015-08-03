a6e428da420c5730231bad72e06ec26b143f07be
EWannerberg <daplff@gmail.com>
2015-08-03 12:59:02 +0200

    wrote a categorizer to make lists from indices

---
+++ VoxelListCategorizer.cpp
@@ -0,0 +1,63 @@
+/*
+ * VoxelListCategorizer.cpp
+ *
+ *  Created on: 3 Aug 2015
+ *      Author: erik
+ */
+
+#include "VoxelListCategorizer.h"
+
+VoxelListCategorizer::VoxelListCategorizer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VoxelListCategorizer::~VoxelListCategorizer() {
+	// TODO Auto-generated destructor stub
+}
+
+void VoxelListCategorizer::addCellToLists(int cellIndex, VoxelCategories cellFlag) {
+
+
+
+	switch(cellFlag)
+	{
+	case FIXED_X:
+		fixedIndicesX.push_back(cellIndex);
+		break;
+	case FIXED_Y:
+		fixedIndicesY.push_back(cellIndex);
+		break;
+	case FIXED_Z:
+		fixedIndicesZ.push_back(cellIndex);
+		break;
+	case LOADED_X:
+		loadedIndicesX.push_back(cellIndex);
+		break;
+	case LOADED_Y:
+		loadedIndicesY.push_back(cellIndex);
+		break;
+	case LOADED_Z:
+		loadedIndicesZ.push_back(cellIndex);
+		break;
+	case ACTIVE:
+		activeIndices.push_back(cellIndex);
+		break;
+	case PASSIVE:
+		passiveIndices.push_back(cellIndex);
+		break;
+	default:
+		break;
+	}
+
+}
+
+void VoxelListCategorizer::readArrayOfCells(std::vector<int> cellArray) {
+
+	for(int cellIndex = 0; cellIndex < cellArray.size(); ++cellIndex )
+	{
+		addCellToLists(cellIndex,static_cast<VoxelCategories>(cellArray[cellIndex]));
+	}
+}
+
+
---
+++ VoxelListCategorizer.h
@@ -0,0 +1,42 @@
+/*
+ * VoxelListCategorizer.h
+ *
+ *  Created on: 3 Aug 2015
+ *      Author: erik
+ */
+
+#ifndef VOXELLISTCATEGORIZER_H_
+#define VOXELLISTCATEGORIZER_H_
+
+#include <vector>
+#include <list>
+
+//class for reading in the cells to be optimized, sorting them into lists for
+//easy outputting into a topology optimizer input file
+class VoxelListCategorizer {
+public:
+	enum VoxelCategories {FIXED_X, FIXED_Y, FIXED_Z,
+		LOADED_X, LOADED_Y, LOADED_Z,
+		ACTIVE, PASSIVE};
+
+
+	VoxelListCategorizer();
+
+
+
+
+	virtual ~VoxelListCategorizer();
+
+	void addCellToLists(int cellIndex, VoxelCategories cellFlag);
+	void readArrayOfCells(std::vector<int> cellArray);
+
+
+private:
+	std::list<int> fixedIndicesX, fixedIndicesY, fixedIndicesZ;
+	std::list<int> loadedIndicesX, loadedIndicesY, loadedIndicesZ;
+	std::list<int> activeIndices, passiveIndices;
+
+
+};
+
+#endif /* VOXELLISTCATEGORIZER_H_ */
