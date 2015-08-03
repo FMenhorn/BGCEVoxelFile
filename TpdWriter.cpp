d7e1a42d093a77b1eab2d41ac55c075c32081053
Friedrich Menhorn <menhorn@in.tum.de>
2015-08-03 14:18:19 +0200

    added first tpdwriter prototype. fill it up!

---
+++ TpdWriter.cpp
@@ -0,0 +1,18 @@
+/*
+ * TpdWriter.cpp
+ *
+ *  Created on: Aug 3, 2015
+ *      Author: friedrich
+ */
+
+#include "TpdWriter.h"
+
+TpdWriter::TpdWriter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TpdWriter::~TpdWriter() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TpdWriter.h
@@ -0,0 +1,28 @@
+/*
+ * TpdWriter.h
+ *
+ *  Created on: Aug 3, 2015
+ *      Author: friedrich
+ */
+
+#ifndef TPDWRITER_H_
+#define TPDWRITER_H_
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+/*
+ *
+ */
+class TpdWriter {
+public:
+	TpdWriter();
+	virtual ~TpdWriter();
+
+	void writeHeader(std::ofstream &outfile);
+
+	void writeCellList(std::string variableName, std::list<int> cellIndices);
+};
+
+#endif /* TPDWRITER_H_ */
